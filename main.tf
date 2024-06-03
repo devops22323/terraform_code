aa8f3e5dc130a3f0d8b39dfdaffce07abb45642d
Kiran M <devops22323@gmail.com>
2024-06-04 01:02:26 +1000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "azuread_user" "example" {
   user_principal_name = "hitu@ksirichandansai2001gmail.onmicrosoft.com"
-  display_name        = "Hitesh Bhargava        "
+  display_name        = "Hitesh Bhargava"
   mail_nickname       = "hitu"
   password            = random_password.password.result
 }
