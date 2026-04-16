03a6fc13cf509f75ee04bf959dc74a7a7937101c
Kiran M <devops22323@gmail.com>
2026-04-16 18:30:43 +1000

    added rg and kv

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,15 @@
 output "rg_id" {
-    value = azurerm_resource_group.example.id
+    value = azurerm_resource_group.example_rg.id
+}
+
+output "kv_id" {
+    value = azurerm_key_vault.example_kv.id
+}
+
+output "kv_name" {
+    value = azurerm_key_vault.example_kv.name
+}
+
+output "kv_url" {
+    value = azurerm_key_vault.example_kv.vault_uri  
 }
